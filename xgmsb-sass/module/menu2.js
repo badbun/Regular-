d100d389a9242c707cf2db24e89f6bf6b65d61d0
wrb <wrb@servyou.com.cn>
2017-01-03 17:29:58 +0800

    基础环境搭建

---
+++ xgmsb-sass/module/menu2.js
@@ -0,0 +1,12 @@
+/**
+ * Created by wrb on 2017/1/3.
+ */
+(function (SBPage) {
+
+    var menu2 = Regular.extend({
+        template: __inline("../template/menu2.html")
+    });
+
+    SBPage.menu2 = menu2;
+
+})(window.SBPage);
---
+++ xgmsb-sass/module/menu3.js
@@ -0,0 +1,11 @@
+/**
+ * Created by wrb on 2017/1/3.
+ */
+(function (SBPage) {
+    var menu3 = Regular.extend({
+        template: __inline("../template/menu3.html")
+    });
+
+    SBPage.menu3 = menu3;
+
+})(window.SBPage);
